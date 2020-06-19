2ea8187bb70280f40d18b3dc32b1cb50d2509100
DucDung <bebom098@gmail.com>
2020-06-19 23:09:01 +0700

    Updates

---
+++ precache-manifest.311c7c299f5209492f51e8cff9e71ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d0cd11c59b95fc7e62d20584059cf5",
+    "url": "/qlnvdoan/index.html"
+  },
+  {
+    "revision": "c1dbaaef5746e0dd56a6",
+    "url": "/qlnvdoan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63ac77cbf35116b815b4",
+    "url": "/qlnvdoan/static/js/2.f1553bfb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/qlnvdoan/static/js/2.f1553bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1dbaaef5746e0dd56a6",
+    "url": "/qlnvdoan/static/js/main.279fd05f.chunk.js"
+  },
+  {
+    "revision": "94c89340ce7f8a95ada6",
+    "url": "/qlnvdoan/static/js/runtime-main.c3d7f8cc.js"
+  }
+]);
